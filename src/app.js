abc0c5121caa91ebd59a46041f403de9ce30f777
raju99-java <rajudebnath.java@outlook.com>
2020-12-21 11:30:12 +0530

    weather app project

---
+++ src/app.js
@@ -0,0 +1,43 @@
+const path = require ("path");
+const hbs = require("hbs");
+const express = require ('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+
+const static_path = path.join(__dirname,"../public");
+const template_path = path.join(__dirname,"../templates/views");
+const partials_path = path.join(__dirname,"../templates/partials");
+
+
+app.set('view engine', 'hbs');
+app.set('views', template_path);
+hbs.registerPartials(partials_path);
+
+
+
+
+app.use(express.static(static_path));
+
+app.get("/", (req, res)=>{
+    res.render('index');    
+});
+
+app.get("/about", (req, res)=>{
+    res.render('about');    
+});
+
+app.get("/weather", (req, res)=>{
+    res.render('weather');    
+});
+
+app.get("*", (req, res)=>{
+    res.render('error_page', {
+        errorMsg : 'Opps! Page Not Found'
+    });    
+});
+
+
+app.listen(port, ()=>{
+    console.log(`Listening Port No - ${port} ...`);
+});
